ceafed198f22523276a8385a30b2855a91e570ef
alextang809 <62707493+alextang809@users.noreply.github.com>
2021-12-04 14:17:25 +0800

    Create maxProfit.java
    sample solution for 121. Best Time to Buy and Sell Stock using dp

---
+++ maxProfit.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        // base case: dp[-1][0] = 0, dp[-1][1] = -infinity
+        int dp_i_0 = 0, dp_i_1 = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            // dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+            // dp[i][1] = max(dp[i-1][1], -prices[i])
+            dp_i_1 = Math.max(dp_i_1, -prices[i]);
+        }
+        return dp_i_0;
+    }
+}
+/*
+Test results
+
+Runtime: 2 ms, faster than 70.76% of Java online submissions for Best Time to Buy and Sell Stock.
+Memory Usage: 51.6 MB, less than 98.11% of Java online submissions for Best Time to Buy and Sell Stock.
+*/
